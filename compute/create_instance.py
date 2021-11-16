5587ac723a8e2103bd00e27dfcaf5f289b903beb
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-11-16 15:01:27 +0000

    Create create_instance.py

---
+++ compute/create_instance.py
@@ -0,0 +1,95 @@
+import argparse
+import os
+import time
+
+import googleapiclient.discovery
+from six.moves import input
+
+compute = googleapiclient.discovery.build('compute', 'v1')
+
+
+# [START list_instances]
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items'] if 'items' in result else None
+# [END list_instances]
+
+x = list_instances(compute,'mickey-331609' ,'europe-west2-a')
+print(x)
+
+def create_instance(compute, project, zone, name, bucket):
+    # Get the latest Debian Jessie image.
+    image_response = compute.images().getFromFamily(
+        project='debian-cloud', family='debian-9').execute()
+    source_disk_image = image_response['selfLink']
+
+    # Configure the machine
+    machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
+    # startup_script = open(
+    #     os.path.join(
+    #         os.path.dirname(__file__), 'startup-script.sh'), 'r').read()
+    # image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
+    # image_caption = "Ready for dessert?"
+
+    config = {
+        'name': name,
+        'machineType': machine_type,
+
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }],
+
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': {
+            # 'items': [{
+            #     # Startup script is automatically executed by the
+            #     # instance upon startup.
+            #     'key': 'startup-script',
+            #     'value': startup_script
+            # }, {
+            #     'key': 'url',
+            #     'value': image_url
+            # }, {
+            #     'key': 'text',
+            #     'value': image_caption
+            # }, {
+            #     'key': 'bucket',
+            #     'value': bucket
+            # }]
+        }
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
+
+create_instance(compute,'mickey-331609' ,'europe-west2-a', 'pyvm','pybucket')
+
+
